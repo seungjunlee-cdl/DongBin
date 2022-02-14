52988cfd5edadfd7f04a27376d037381094b8225
Seungjun Lee <56711947+seungjunlee-cdl@users.noreply.github.com>
2022-02-14 14:19:59 +0900

    Update Greedy_3_String_flip.py

--- Greedy_3_String_flip.py
+++ Greedy_3_String_flip.py
@@ -7,3 +7,9 @@ def solution(S):
             cnt += 1
 
     return math.ceil(cnt/2)
+
+# testcase1 = '0001100'   return 1
+# testcase1 = '11111'   return 0
+# testcase1 = '00000001'   return 1
+# testcase1 = '11001100110011000001'   return 4
+# testcase1 = '11101101'   return 2
