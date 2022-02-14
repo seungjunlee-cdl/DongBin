5bcd48fc1427b5a0743b2da271d4ef3a259548b0
Seungjun Lee <56711947+seungjunlee-cdl@users.noreply.github.com>
2022-02-14 15:09:36 +0900

    Update Greedy_5_Bowling_ball.py

--- Greedy_5_Bowling_ball.py
+++ Greedy_5_Bowling_ball.py
@@ -4,3 +4,6 @@ def solution(N,M,balls):
         cnt += N - (i+1) - balls[i+1:].count(balls[i])
     
     return cnt
+
+# testcase1 => N = 5, M = 3, balls = [1,3,2,3,2]   return 8
+# testcase2 => N = 8, M = 5, balls = [1,5,4,3,2,4,5,2]   return 25
