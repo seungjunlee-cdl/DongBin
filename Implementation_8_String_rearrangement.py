4812bafc3d95bdbfe1f834b5fb4e95006290ba64
Seungjun Lee <56711947+seungjunlee-cdl@users.noreply.github.com>
2022-02-14 16:52:55 +0900

    Add files via upload

---
+++ Implementation_8_String_rearrangement.py
@@ -0,0 +1,17 @@
+def solution(S):
+    S_div = list(map(str,S))
+    num_sum = 0
+    sorted_str = []
+    for i in range(len(S_div)):
+        S_div[i] = ord(S_div[i])
+    S_div.sort()
+    for i in range(len(S_div)):
+        if S_div[i] < 65:
+            num_sum += int(chr(S_div[i]))
+        else:
+            sorted_str.append(chr(S_div[i]))
+
+    sorted_str.append(str(num_sum))
+    sorted_str = "".join(sorted_str)
+
+    return sorted_str
